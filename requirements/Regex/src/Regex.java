78905efd6eb87755b50fc43c70f294c702b8c95c
Arian <arianmohseni1382@gmail.com>
2023-05-16 02:43:33 +0330

    function for Regex added

--- requirements/Regex/src/Regex.java
+++ requirements/Regex/src/Regex.java
@@ -17,28 +17,6 @@ public class Regex {
 
     public static void main(String[] args) {
 
-        // ArrayList<String> emails = new ArrayList<>();
-
-        // //valid types
-        // emails.add("user@domain.com");
-        // emails.add("user@domain.co.in");
-        // emails.add("user1@domain.com");
-        // emails.add("user.name@domain.com");
-        // emails.add("user_name@domain.co.in");
-        // emails.add("user-name@domain.co.in");
-        // emails.add("user@domaincom");
-        // emails.add("kirekhar@aut.ac.ir");
-
-        // //invalid types
-        // emails.add("@yahoo.com");
-        // String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
-        // Pattern pattern = Pattern.compile(regex);
-
-        // for (String email : emails) {
-        //     Matcher matcher = pattern.matcher(email);
-        //     System.out.println(email + " : " + matcher.matches());
-        // }
-
         String test = "salam@gmail.com";
 
         if(isValidEmail(test)){
