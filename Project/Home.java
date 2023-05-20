395a7d830b11b3ad67dad11f319ba55d8b3e123e
Arian <arianmohseni1382@gmail.com>
2023-05-20 20:02:43 +0330

    the start point of the program (you should change the path here)

---
+++ Project/Home.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+
+
+        //data base manager method 
+        
+        /* ADD THE CORRECT PATH BEFORE USING THE CODE
+        * Add the path where you've cloned this repo 
+        * example : [Your_Path]/AP_Project/Project/projectDataBase.db
+        * just add the [Your_Path] part in the string
+        */
+        String path = "Java";
+        
+        databasemanage.DataBaseManagement(path);
+        
+    }
+}
