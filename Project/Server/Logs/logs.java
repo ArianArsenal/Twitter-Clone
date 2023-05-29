8b5b1c4beeb716bfb4e5c433ec5901b2c317ec11
Arian <arianmohseni1382@gmail.com>
2023-05-30 00:01:29 +0330

    edited

--- Project/Server/Logs/logs.java
+++ Project/Server/Logs/logs.java
@@ -2,30 +2,38 @@ package Server.Logs;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class logs {
 
-    public static void SignUpLog(String username) { //logs when the user is created
-        // Get the current system time (hour and minute)
-        LocalTime currentTime = LocalTime.now();
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
-        String logMessage = "User Created: " + username + " | Create Time: " + currentTime;
+    public static void SignUpLog(String username) {
+        LocalTime currentTime = LocalTime.now();
+        LocalDate currentDate = LocalDate.now();
+
+        String logMessage = "User Created: " + username + " | Create Time: " + formatTime(currentTime) + " " + currentDate;
         writeLog(logMessage);
     }
 
-    public static void SignInLog(String username) {  //logs when the user is signed in
-        // Get the current system time (hour and minute)
+    public static void SignInLog(String username) {
         LocalTime currentTime = LocalTime.now();
+        LocalDate currentDate = LocalDate.now();
 
-        String logMessage = "User Signed In: " + username + " | Sign-In Time: " + currentTime;
+        String logMessage = "User Signed In: " + username + " | Sign-In Time: " + formatTime(currentTime) + " " + currentDate;
         writeLog(logMessage);
+    }
+
+    private static String formatTime(LocalTime time) {
+        return time.format(TIME_FORMATTER);
     }
 
     private static void writeLog(String logMessage) {
         try {
             String filePath = "C:/Users/Arian/OneDrive/Desktop/Progs/Java/AP_Project/Project/Server/Logs/UsersLogs.txt";
-            FileWriter fileWriter = new FileWriter(filePath, true); // append to the file
+            FileWriter fileWriter = new FileWriter(filePath, true);
             fileWriter.write(logMessage + "\n");
             fileWriter.close();
         } catch (IOException e) {
